c4a3f305f50ee9bc126e5fd71e8d198c085ed187
Tony <fc0302@naver.com>
2023-07-28 14:23:03 +0700

    Initial Commit

---
+++ lck_project/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  lck_project
+//
+//  Created by Tony on 2023/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        VStack {
+            NavigationView {
+                VStack {
+                    Image("lck_icon")
+                        .resizable()
+                        .frame(width: 210, height: 120)
+                        .padding(40)
+                    Text("League of Legends Championship Korea")
+                        .font(Font.custom("CHANEY-UltraExtended", size: 20, relativeTo: .title2))
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Image("lol_icon")
+                        .resizable()
+                        .frame(width: 70, height: 70)
+                    Spacer()
+                    Text("Be the MYTH\nBe the LEGEND")
+                        .font(Font.custom("SinkinSans-500Medium", size: 20, relativeTo: .title2))
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(.center)
+                        .padding(30)
+                        .lineSpacing(7
+                        )
+                    Spacer()
+                    Text("Let's watch LCK!")
+                        .font(Font.custom("SinkinSans-500Medium", size: 15, relativeTo: .body))
+                        .padding(25)
+                    NavigationLink(destination: teamList()) {
+                        Image(systemName: "arrowtriangle.right.circle.fill").resizable()
+                            .frame(width: 60, height: 60)
+                    }
+                    Button(action: {
+                        showingAlert = true
+                    }, label: {
+                        Image(systemName: "info.circle")
+                    }).alert(isPresented: $showingAlert) {
+                            Alert(title: Text("LCK Introducing app"), message: Text("s3916884: Junsik Kang"), dismissButton: .default(Text("Got it!")))
+                        }.offset(x: 160, y: 5)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lck_project/lck_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  lck_projectApp.swift
+//  lck_project
+//
+//  Created by Tony on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct lck_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
